a2342f6e9d3ab750c25238d76d9d0a7b5693c7e1
vipyan <vipinkaniyanthara@gmail.com>
2025-04-13 08:09:28 -0400

    Updated dates

--- JS/data.js
+++ JS/data.js
@@ -2,8 +2,8 @@ const blogData = [
     {
         imageSrc: "../images/article-image-03.png",
         date: {
-            datetime: "2023-10-27",
-            display: "October 27, 2023",
+            datetime: "2024-10-27",
+            display: "October 27, 2024",
         },
         heading: "Blog one",
         content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
@@ -11,8 +11,8 @@ const blogData = [
     {
         imageSrc: "../images/article-image-02.png",
         date: {
-            datetime: "2023-10-27",
-            display: "October 27, 2023",
+            datetime: "2024-10-27",
+            display: "October 27, 2024",
         },
         heading: "Blog two",
         content: "Scrimba has given me a lot of confidence because they provide many questions and puzzles to be solved, which gives me valuable hands-on experience.",
@@ -20,8 +20,8 @@ const blogData = [
     {
         imageSrc: "../images/article-image-01.png",
         date: {
-            datetime: "2023-10-27",
-            display: "October 27, 2023",
+            datetime: "2024-10-27",
+            display: "October 27, 2024",
         },
         heading: "Blog three",
         content: "I'm 50% confident that I can participate in any interview for a frontend developer role.",
@@ -30,8 +30,8 @@ const blogData = [
       {
         imageSrc: "../images/blog-image-01.png",
         date: {
-            datetime: "2023-10-28",
-            display: "October 28, 2023",
+            datetime: "2024-10-28",
+            display: "October 28, 2024",
         },
         heading: "Blog four",
         content: "Today I explored innovative CSS layouts and responsive design techniques that can significantly improve user experience on various devices.",
@@ -39,8 +39,8 @@ const blogData = [
     {
         imageSrc: "../images/blog-image-02.png",
         date: {
-            datetime: "2023-10-29",
-            display: "October 29, 2023",
+            datetime: "2024-10-29",
+            display: "October 29, 2024",
         },
         heading: "Blog five",
         content: "I experimented with several JavaScript libraries to create dynamic interactions, and the results opened up new ways to enhance website performance.",
@@ -48,8 +48,8 @@ const blogData = [
     {
         imageSrc: "../images/blog-image-03.png",
         date: {
-            datetime: "2023-10-30",
-            display: "October 30, 2023",
+            datetime: "2024-10-30",
+            display: "October 30, 2024",
         },
         heading: "Blog six",
         content: "In this post, I share my thoughts on sustainable web development practices and how eco-friendly coding principles are emerging trends in the tech community.",
